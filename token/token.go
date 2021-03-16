b04103003902dd1744068211d763c473d08b71cd
Femi Ojo <femiojo24@gmail.com>
2021-03-16 22:44:39 +0100

    Add lexer with tests

--- token/token.go
+++ token/token.go
@@ -28,6 +28,18 @@ const (
 	LET      = "LET"
 )
 
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
+
 type Token struct {
 	Type    TokenType
 	Literal string
