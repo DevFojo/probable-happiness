cd495a65383ca2156ed5379d2b382742346df044
Femi Ojo <femiojo24@gmail.com>
2021-03-16 23:09:48 +0100

    Add support for multi character tokens

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -25,12 +25,26 @@ func (l *Lexer) readChar() {
 	l.readPosition += 1
 }
 
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
+}
+
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 	l.skipWhitespace()
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.EQ, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
 	case '(':
@@ -44,7 +58,13 @@ func (l *Lexer) NextToken() token.Token {
 	case '-':
 		tok = newToken(token.MINUS, l.ch)
 	case '!':
-		tok = newToken(token.BANG, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.NOT_EQ, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
 	case '/':
 		tok = newToken(token.SLASH, l.ch)
 	case '*':
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -166,3 +166,32 @@ func TestNextToken_Conditionals(t *testing.T) {
 		}
 	}
 }
+
+func TestNextToken_MultiCharOperators(t *testing.T) {
+	input := `5 == 15;
+12 != 12;
+`
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.INT, "5"},
+		{token.EQ, "=="},
+		{token.INT, "15"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "12"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "12"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q,got=%q", i, tt.expectedType, tok.Type)
+		}
+	}
+}
