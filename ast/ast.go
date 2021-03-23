0c66fa84677c40be875ab4ab3c0c0373c0bb58dc
Femi Ojo <femiojo24@gmail.com>
2021-03-23 09:34:24 +0100

    Add initial ast implementation

---
+++ ast/ast.go
@@ -0,0 +1,50 @@
+package ast
+
+import "github.com/devfojo/ph/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+	return ""
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
+
+func (i *Identifier) expressionNode() {}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (l *LetStatement) TokenLiteral() string {
+	return l.Token.Literal
+}
+
+func (l *LetStatement) statementNode() {}
